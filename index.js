d54f0872446593695c65327d6ef83712ce7c1f75
Marcos Akihito Sato <114444361+mrcosato@users.noreply.github.com>
2023-01-28 10:21:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/*var i = 1;
+while (i <= 10){
+    console.log("Alö mundo!!!");
+    i= i +1;
+}
+console.log('Acabou o programa');*/
+var readline = require('readline-sync');
+var i=0;
+for (var i= 0; i <= 10; i++){
+    console.log("Alö mundo!!!");
+}
